7131ee75bd796fed21604d218bd3b0737a675ce2
Cheikh Seck <jvmstix@gmail.com>
2022-07-18 17:23:56 +0200

    Fix autocomplete
    Fix autocomplete

--- api/handlers/rest_ApiComplete.go
+++ api/handlers/rest_ApiComplete.go
@@ -13,7 +13,6 @@ import (
 	"github.com/cheikhshift/gos/core"
 	"github.com/gorilla/sessions"
 	"github.com/thestrukture/IDE/api/globals"
-	"gopkg.in/mgo.v2/bson"
 )
 
 //
@@ -21,87 +20,25 @@ func ApiComplete(w http.ResponseWriter, r *http.Request, session *sessions.Sessi
 
 	prefx := r.FormValue("pref")
 
-	ret := []bson.M{}
-	//return {name: ea.word, value: ea.insert, score: 0, meta: ea.meta}
-	gxml := os.ExpandEnv("$GOPATH") + "/src/" + r.FormValue("pkg") + "/gos.gxml"
+	content := r.FormValue("content")
+	id := r.FormValue("id")
+	tempFile := filepath.Join(globals.AutocompletePath, id)
 
-	if _, err := os.Stat(gxml); !os.IsNotExist(err) && r.FormValue("gocode") == "" {
-		gos, _ := core.PLoadGos(gxml)
-		score := 0
-		for _, v := range gos.Variables {
+	ioutil.WriteFile(tempFile, []byte(content), 0700)
+	cms := "gocode"
 
-			if strings.Contains(v.Name, prefx) {
-				score = score + 1
-				ret = append(ret, bson.M{"name": v.Name, "value": v.Name, "score": score, "meta": "Global variable | " + v.Type})
-			}
-
-		}
-
-		for _, v := range gos.RootImports {
-
-			if strings.Contains(v.Src, prefx) {
-				score = score + 1
-				paths := strings.Split(v.Src, "/")
-				ret = append(ret, bson.M{"name": v.Src, "value": paths[len(paths)-1], "score": score, "meta": "package"})
-			}
-
-		}
-
-		for _, v := range gos.Header.Structs {
-
-			if strings.Contains(v.Name, prefx) {
-				score = score + 1
-				ret = append(ret, bson.M{"name": v.Name, "value": v.Name, "score": score, "meta": "Interface"})
-			}
-
-		}
-
-		for _, v := range gos.Header.Objects {
-
-			if strings.Contains(v.Name, prefx) {
-				score = score + 1
-				ret = append(ret, bson.M{"name": v.Name, "value": v.Name, "score": score, "meta": "{{Interface func group}}"})
-			}
-
-		}
-
-		for _, v := range gos.Methods.Methods {
-
-			if strings.Contains(v.Name, prefx) {
-				score = score + 1
-				ret = append(ret, bson.M{"name": v.Name, "value": v.Name + " ", "score": score, "meta": "{{Template pipeline}}"})
-				score = score + 1
-				ret = append(ret, bson.M{"name": v.Name, "value": "net" + v.Name + "(" + v.Variables + ")", "score": score, "meta": "Pipeline go function."})
-			}
-
-		}
-
-		for _, v := range gos.Templates.Templates {
-
-			if strings.Contains(v.Name, prefx) {
-				score = score + 1
-				ret = append(ret, bson.M{"name": v.Name, "value": v.Name + " ", "score": score, "meta": "{{Template $struct}}"})
-				score = score + 1
-				ret = append(ret, bson.M{"name": v.Name, "value": "netb" + v.Name + "(" + v.Struct + "{})", "score": score, "meta": "Template build go function."})
-			}
-
-		}
-
-		response = mResponse(ret)
-	} else {
-		content := r.FormValue("content")
-		id := r.FormValue("id")
-		tempFile := filepath.Join(globals.AutocompletePath, id)
-
-		ioutil.WriteFile(tempFile, []byte(content), 0700)
-		cmd := fmt.Sprintf("gocode -f=json --in=%s autocomplete %s", tempFile, prefx)
-
-		res, _ := core.RunCmdSmart(cmd)
-		response = res
-
-		os.Remove(tempFile)
-
+	if globals.Windows {
+		cms = strings.Replace(os.ExpandEnv("$GOPATH")+"\\bin\\gocode.exe", "/", "\\", -1)
 	}
+
+	cmd := fmt.Sprintf(cms+" -f=json --in=%s autocomplete %s", tempFile, prefx)
+
+	res, err := core.RunCmdSmart(cmd)
+	response = res
+
+	
+
+	os.Remove(tempFile)
 
 	callmet = true
 	return
