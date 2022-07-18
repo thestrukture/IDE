61e2fad313ae4346aefb32a8cdefe10629a58a25
Cheikh Seck <jvmstix@gmail.com>
2022-07-18 21:04:37 +0200

    fix build error caused by unused variable.

--- api/handlers/rest_ApiComplete.go
+++ api/handlers/rest_ApiComplete.go
@@ -33,7 +33,7 @@ func ApiComplete(w http.ResponseWriter, r *http.Request, session *sessions.Sessi
 
 	cmd := fmt.Sprintf(cms+" -f=json --in=%s autocomplete %s", tempFile, prefx)
 
-	res, err := core.RunCmdSmart(cmd)
+	res,_ := core.RunCmdSmart(cmd)
 	response = res
 
 	
